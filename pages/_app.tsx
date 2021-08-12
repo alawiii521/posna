83d3c84a1d462a0e531e74feb253a634825d83e1
Ali Majed <ali.majed.am@hotmail.se>
2021-08-12 12:17:21 +0200

    app global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { GlobalStyle } from "@portion/style";
+import {
+  GlobalStyle as PortionGlobalStyle,
+  PortionCssProp,
+} from "@portion/style";
+import { createGlobalStyle, css } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`${css`
+  body {
+    font-family: "Ubuntu", sans-serif;
+    margin: 0;
+    padding: 0;
+    background: var(${PortionCssProp.color.background.primary.name});
+    color: var(${PortionCssProp.color.font.primary.name});
+  }
+`}`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <PortionGlobalStyle />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
